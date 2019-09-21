586d1b074344e302dfcf88eccb672b754ce3589f
Roy Tang <roytang.rb@gmail.com>
2019-09-21 23:53:54 +0800

    vietnam map desktop

--- src/modules/Routes/routes-ctrl.js
+++ src/modules/Routes/routes-ctrl.js
@@ -3,6 +3,7 @@ import Malaysia from './malaysia.js'
 import Indonesia from './indonesia.js'
 import Nepal from './nepal.js'
 import HK from './hk.js'
+import Vietnam from './vietnam.js'
 
 const RoutesCtrl = {}
 var	initCalledOnce = false
@@ -20,7 +21,7 @@ RoutesCtrl.init = (isDesktop) => {
 	}
   if (isDesktop){
     //return all controllers
-    return [Malaysia.MapCtrl1(), Malaysia.MapCtrl2(), Nepal.MapCtrl(), HK.MapCtrl()] //Cameroon.MapCtrl1(),
+    return [Malaysia.MapCtrl1(), Malaysia.MapCtrl2(), Nepal.MapCtrl(), HK.MapCtrl(), Vietnam.MapCtrl()] //Cameroon.MapCtrl1(),
   } else {
     return [Malaysia.MapCtrlM1(), Malaysia.MapCtrlM2(), Malaysia.MapCtrlM3(), Indonesia.MapCtrlM1(), Nepal.MapCtrlM(), HK.MapCtrlM()]
   }
