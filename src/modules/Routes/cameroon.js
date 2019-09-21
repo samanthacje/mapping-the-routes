615aa55c321877792970401d65955f64d1c40e95
Yasmin Zulhaime <ainayasminmpm@gmail.com>
2019-09-21 19:19:20 +0800

    Cameroon map pinned, still not changing, js trial failed

--- src/modules/Routes/cameroon.js
+++ src/modules/Routes/cameroon.js
@@ -42,8 +42,11 @@ Cameroon.MapCtrl1 = () => {
   var first_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (-navOffset), reverse: true})
     .on('enter', () =>
       new TimelineMax()
-        .add(TweenMax.to($('#arrows'), 0.5, visiable_opt))
-        .add(TweenMax.to($('#melaka-label'), 0.5, visiable_opt))
+        .add(TweenMax.to($('#bertoua-text'), 0.5, visiable_opt))
+        .add(TweenMax.to($('#abong-text'), 0.5, visiable_opt))
+        .add(TweenMax.to($('#mindourou-text'), 0.5, visiable_opt))
+        .add(TweenMax.to($('#lomie-text'), 0.5, visiable_opt))
+        .add(TweenMax.to($('#djoum-text'), 0.5, visiable_opt))
     )
     .on('leave', () => {
       var targets = [$('#arrows'), $('#melaka-label')]
@@ -55,8 +58,8 @@ Cameroon.MapCtrl1 = () => {
   var second_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (duration_per-navOffset), reverse: true})
     .on('enter', () =>
       new TimelineMax()
-        .add(TweenMax.to($('#forest-fill'), 0.5, visiable_opt))
-        .add(TweenMax.to($('#forest-label'), 0.3, visiable_opt))
+        .add(TweenMax.to($('#arrow6'), 0.5, visiable_opt))
+        .add(TweenMax.to($('#arrow7'), 0.5, visiable_opt))
     )
     .on('leave', () => {
       var targets = [$('#forest-fill'), $('#forest-label')]
@@ -68,223 +71,19 @@ Cameroon.MapCtrl1 = () => {
   var thrid_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (2 * duration_per-navOffset), reverse: true})
     .setTween(
       new TimelineMax()
-        .add(TweenMax.to($('#highway-line'), 0.7, {strokeDashoffset: 0, ease:Linear.easeNone}))
         .add(TweenMax.to([
-          $('#bukit-text'), $('#marker-bukit')
+          $('#marker-yairport'), $('#marker-dairport'),$('#arrow1'), $('#arrow2'), $('#arrow3'), $('#arrow4'),$('#arrow5'), $('#yairport-text'),$('#dairport-txt')
           ], 0, visiable_opt), 0.7)
         .addCallback(()=>{
           pulseMarker([
-            $('#marker-bukit'),
+            $('#marker-yairport'),
+            $('#marker-dairport'),
           ])
         }, 0.7)
-
-        .add(TweenMax.to([
-          $('#marker-rantau'), $('#marker-padang'), $('#padang-text'), $('#rantau-text')
-          ], 0, visiable_opt), 0.8)
-        .addCallback(()=>{
-          pulseMarker([
-            $('#marker-rantau'),
-            $('#marker-padang'),
-          ])
-        }, 0.8)
     )
     .addTo(controller);
 
     return controller
-}
-
-Cameroon.MapCtrl2 = () => {
-  var map = Cameroon.maps[1],
-    visiable_opt = {opacity: 1, ease:Linear.easeNone},
-    hidden_opt = {opacity: 0, ease:Linear.easeNone}
-
-  var controller = new ScrollMagic.Controller();
-
-  var navOffset = ($('.nav').offsetHeight + $('#progress-div').offsetHeight) || 0
-
-  var pin_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: '400%', offset: (-navOffset), reverse: true})
-    .on('enter', function(){
-      this.setPin(this.triggerElement(), {pushFollowers: false})
-    })
-    .addTo(controller)
-
-  var duration_per = pin_scene.duration() / 4
-
-  var first_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (-navOffset), reverse: true})
-    .on('start', (e) =>{
-      var isForward = e.scrollDirection == 'FORWARD' ? true : false
-      if (isForward){
-        new TimelineMax()
-          .add(TweenMax.to($('#marker-bukit-2'), 0.5, visiable_opt))
-          .add(TweenMax.to($('#bukit-text-2'), 0.5, visiable_opt))
-          .addCallback(()=>{
-            pulseMarker([
-              $('#marker-bukit-2')
-            ])
-          }, 1)
-      } else {
-        var targets = [$('#marker-bukit-2'), $('#bukit-text-2')]
-        TweenMax.killTweensOf(targets)
-        TweenMax.to(targets, 0.5, hidden_opt)
-      }
-    })
-    .addTo(controller);
-
-  var second_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (duration_per-navOffset), reverse: true})
-    .on('start', (e) =>{
-      var isForward = e.scrollDirection == 'FORWARD' ? true : false
-      if (isForward){
-        new TimelineMax()
-          .add(TweenMax.to($('#marker-alor'), 0.5, visiable_opt))
-          .add(TweenMax.to($('#alor-text'), 0.5, visiable_opt))
-          .addCallback(()=>{
-            pulseMarker([
-              $('#marker-alor')
-            ])
-          }, 1)
-      } else {
-        var targets = [$('#marker-alor'), $('#alor-text')]
-        TweenMax.killTweensOf(targets)
-        TweenMax.to(targets, 0.5, hidden_opt)
-      }
-    })
-    .addTo(controller);
-
-  var thrid_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (2*duration_per-navOffset), reverse: true})
-    .on('start', (e) =>{
-      var isForward = e.scrollDirection == 'FORWARD' ? true : false
-      if (isForward){
-        new TimelineMax()
-          .add(TweenMax.to($('#marker-sadao'), 0.5, visiable_opt))
-          .add(TweenMax.to($('#sadao-text'), 0.5, visiable_opt))
-          .addCallback(()=>{
-            pulseMarker([
-              $('#marker-sadao')
-            ])
-          }, 1)
-      } else {
-        var targets = [$('#marker-sadao'), $('#sadao-text')]
-        TweenMax.killTweensOf(targets)
-        TweenMax.to(targets, 0.5, hidden_opt)
-      }
-    })
-    .addTo(controller);
-
-  var forth_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (3*duration_per-navOffset), reverse: true})
-    .on('start', (e) =>{
-      var isForward = e.scrollDirection == 'FORWARD' ? true : false
-      if (isForward){
-        new TimelineMax()
-          .add(TweenMax.to($('#marker-police'), 0.5, visiable_opt))
-          .add(TweenMax.to($('#kedahpolice-text'), 0.5, visiable_opt))
-          .addCallback(()=>{
-            pulseMarker([
-              $('#marker-police')
-            ])
-          }, 1)
-      } else {
-        var targets = [$('#marker-police'), $('#kedahpolice-text')]
-        TweenMax.killTweensOf(targets)
-        TweenMax.to(targets, 0.5, hidden_opt)
-      }
-    })
-    .addTo(controller);
-
-  return controller
-}
-
-Cameroon.MapCtrlM1 = () => {
-  var map = Cameroon.maps_m[0]
-
-  var controller = new ScrollMagic.Controller();
-
-  var navOffset = ($('.nav').offsetHeight + $('#progress-div').offsetHeight) || 0
-
-  var pin_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: '300%', offset: (-navOffset), reverse: true})
-    .on('enter', function(){
-      this.setPin(this.triggerElement(), {pushFollowers: false})
-    })
-    .addTo(controller)
-
-  var duration_per = pin_scene.duration() / 3
-  //first scene trigger inmediately on map pin, use onLeave triggerHook
-  var second_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (duration_per-navOffset), reverse: true})
-    .on('start', (e) => { switchByDir(e, map, Cameroon.pics_m[1], Cameroon.pics_m[0]) })
-    .addTo(controller);
-
-  var thrid_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (2*duration_per-navOffset), reverse: true})
-    .on('start', (e) => { switchByDir(e, map, Cameroon.pics_m[2], Cameroon.pics_m[1]) })
-    .addTo(controller);
-
-  return controller
-}
-
-Cameroon.MapCtrlM2 = () => {
-  var map = Cameroon.maps_m[1]
-
-  var controller = new ScrollMagic.Controller();
-
-  var navOffset = ($('.nav').offsetHeight + $('#progress-div').offsetHeight) || 0
-
-  var pin_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: '400%', offset: (-navOffset), reverse: true})
-    .on('enter', function(){
-      this.setPin(this.triggerElement(), {pushFollowers: false})
-    })
-    .addTo(controller)
-
-  var duration_per = pin_scene.duration() / 4
-  //first scene trigger inmediately on map pin, use onLeave triggerHook
-  var second_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (duration_per-navOffset), reverse: true})
-    .on('start', (e) => { switchByDir(e, map, Cameroon.pics_m[4], Cameroon.pics_m[3]) })
-    .addTo(controller);
-
-  var thrid_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (2*duration_per-navOffset), reverse: true})
-    .on('start', (e) => { switchByDir(e, map, Cameroon.pics_m[5], Cameroon.pics_m[4]) })
-    .addTo(controller);
-
-  var forth_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: duration_per, offset: (3*duration_per-navOffset), reverse: true})
-    .on('start', (e) => { switchByDir(e, map, Cameroon.pics_m[6], Cameroon.pics_m[5]) })
-    .addTo(controller);
-
-  return controller
-}
-
-Cameroon.MapCtrlM3 = () => {
-  var map = Cameroon.maps_m[2]
-
-  var controller = new ScrollMagic.Controller();
-
-  var navOffset = ($('.nav').offsetHeight + $('#progress-div').offsetHeight) || 0
-
-  var pin_scene = new ScrollMagic.Scene({ triggerElement: map, triggerHook:'onLeave', duration: '100%', offset: (-navOffset), reverse: true})
-    .on('enter', function(){
-      this.setPin(this.triggerElement(), {pushFollowers: false})
-    })
-    .addTo(controller)
-
-  return controller
-}
-
-function pulseMarker(markers){
-	var inners = markers.map(item=>item.querySelector('.marker-inner'))
-	var outters = markers.map(item=>item.querySelector('.marker-outter'))
-
-	// TweenMax.fromTo(inners, 3, {attr:{r: 12.06}}, {attr:{r: 20}}).repeat(-1)//.yoyo(true)
-	TweenMax.fromTo(outters, 1, {attr:{r: 20}, opacity: 0.4}, {attr:{r: 55.68}, opacity: 0}).repeat(-1)
-}
-
-function changeBg($map, src){
-  //used only on mobile
-	$map.querySelector('.routes-map-bg-m').src = src
-}
-
-function switchByDir(e, $map, src_old, src){
-  var isForward = e.scrollDirection == 'FORWARD' ? true : false
-    if (isForward){
-      changeBg($map, src)
-    } else {
-      changeBg($map, src_old)
-    }
 }
 
 export default Cameroon
--- src/modules/Routes/routes-ctrl.js
+++ src/modules/Routes/routes-ctrl.js
@@ -1,11 +1,12 @@
 import Malaysia from './malaysia.js'
+import Cameroon from './cameroon.js'
 
 const RoutesCtrl = {}
 
 RoutesCtrl.init = (isDesktop) => {
   if (isDesktop){
     //return all controllers
-    return [Malaysia.MapCtrl1(), Malaysia.MapCtrl2()]
+    return [Malaysia.MapCtrl1(), Malaysia.MapCtrl2(), Cameroon.MapCtrl1()]
   } else {
     return [Malaysia.MapCtrlM1(), Malaysia.MapCtrlM2(), Malaysia.MapCtrlM3()]
   }
